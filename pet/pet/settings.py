5ab49e0ab7c0c972b9a1196777fe8193239182c0
Andrew Ivanov <andrew11.12.97@mail.ru>
2024-05-19 20:05:02 +0300

    add redis cache

--- pet/pet/settings.py
+++ pet/pet/settings.py
@@ -129,3 +129,12 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MOEX_ALL_SESSION_ID = 'moex_all'
+
+
+CHACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379',
+        'TIMEOUT': 300,
+    }
+}
--- pet/shares/views.py
+++ pet/shares/views.py
@@ -4,6 +4,7 @@ from .models import Security
 from analys.forms import MovingAveragesForm
 from analys.models import MovingAverages
 from analys.analys import analys_plot
+from django.core.cache import cache
 # Create your views here.
 
 def title_page(request):
@@ -12,7 +13,12 @@ def title_page(request):
                   {'securities': securities})
 
 def security_detail(request, slug):
-    security = get_object_or_404(Security, slug = slug)
+    key = f'{slug}'
+    security = cache.get(key)
+    if not security:
+        security = get_object_or_404(Security, slug = slug)
+        cache.set(key, security)
+   
     price, volume = security.price, security.volume
     fig_price = analys_plot(price, volume, items = MovingAverages.objects.all())
     line_price = fig_price.to_html(full_html = False, include_plotlyjs = False)
